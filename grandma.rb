e5df6bb0767e47500cec45a167820909c0caf7b6
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-07 22:12:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,11 @@
 def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+     puts "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
-    puts "NO, NO SINCE 1938!"
+      puts "NO, NO SINCE 1938!"
     else
-    puts "HUH?! SPEAK UP SONNY"
+      puts "HUH?! SPEAK UP SONNY"
   end
 end
 
